9d17d7209a5061aadf6db0b2fcb9b68b2c717594
brettonw <bretton.wade@gmail.com>
2018-08-01 19:48:40 +0000

    export the project list in dependency order

--- bin/getcontextvars.pl
+++ bin/getcontextvars.pl
@@ -31,23 +31,69 @@ Context::addTypeNamed("project", $ContextType{VALUES},
     )
 );
 
-if ($ARGV[0] eq "targets") {
-    my $sourcePath = Context::confType("project", $ContextType{VALUES}, "sourcePath");
-    if (opendir(SOURCE_PATH, $sourcePath)) {
-        my $separator = "";
-        while (my $target = readdir(SOURCE_PATH)) {
-            # skip unless $target is a non-hidden directory, that is not also the build directory
-            next unless (($target !~ /^\./) && (-d "$sourcePath/$target"));
-            next if (abs_path("$sourcePath/$target") eq abs_path(Context::confType("project", $ContextType{VALUES}, "buildPath")));
-            print "$separator$target";
-            $separator = ",";
+#---------------------------------------------------------------------------------------------------
+# phase 2 - read the source path looking for subdirs, and loading their contexts
+#---------------------------------------------------------------------------------------------------
+my $targets = {};
+my $targetPrefix = "#";
+my $sourcePath = Context::confType ("project", $ContextType{VALUES}, "sourcePath");
+
+if (opendir(SOURCE_PATH, $sourcePath)) {
+    while (my $target = readdir(SOURCE_PATH)) {
+        # skip unless $target is a non-hidden directory, that is not also the build directory
+        next unless (($target !~ /^\./) && (-d "$sourcePath/$target"));
+        next if (abs_path("$sourcePath/$target") eq abs_path(Context::confType ("project", $ContextType{VALUES}, "buildPath")));
+
+        # load the target context to get the dependencies
+        Context::load ("$targetPrefix$target", "$sourcePath/$target/");
+        my $targetContext = Context::concatenateNamed ("project-" . $ContextType{VALUES}, "$targetPrefix$target-" . $ContextType{VALUES});
+        $targets->{$target} = $targetContext;
+    }
+    closedir(SOURCE_PATH);
+} else {
+    print STDERR "Can't open source directory ($sourcePath), $!\n";
+    exit ($!);
+}
+
+#---------------------------------------------------------------------------------------------------
+# phase 3 - identify the targets to build, and the dependency order to do it
+#---------------------------------------------------------------------------------------------------
+# the dependency graph is implicit in the dependencies array for each target, so we traverse it in
+# depth first order, emitting build targets on return (and marking them as "touched" so we don't
+# emit them twice).
+sub traverseTargetDependencies {
+    my ($targetDependencies, $target, $forTarget) = @_;
+    if (exists ($targets->{$target})) {
+        if (!exists($targets->{$target}->{touched})) {
+            $targets->{$target}->{touched} = 1;
+            my $dependencies = exists($targets->{$target}->{dependencies}) ? $targets->{$target}->{dependencies} : [];
+            for my $dependency (sort (@$dependencies)) {
+                traverseTargetDependencies($targetDependencies, $dependency, $forTarget);
+            }
+            push (@$targetDependencies, $target);
         }
-        print "\n";
-        closedir(SOURCE_PATH);
+    } else {
+        print STDERR "Dependency ($target) of ($forTarget) is not a known target.\n";
+        exit (1);
     }
-    else {
-        print STDERR "Can't open source directory ($sourcePath), $!\n";
+}
+
+# traverse the dependencies for all the targets into one list
+my $targetsToBuild = [sort keys (%$targets)];
+my $targetsInDependencyOrder = [];
+for my $target (@$targetsToBuild) {
+    traverseTargetDependencies ($targetsInDependencyOrder, $target, $target);
+}
+
+#---------------------------------------------------------------------------------------------------
+if ($ARGV[0] eq "targets") {
+    # get the targets, as sorted in dependency order
+    my $separator = "";
+    for my $target (@$targetsInDependencyOrder) {
+        print "$separator$target";
+        $separator = ",";
     }
+    print "\n";
 } elsif ($ARGV[0] eq "configurations") {
     my $configurationsHash = Context::concatenate (
         Context::getTypeNamed("root", $ContextType{CONFIGURATIONS}),
